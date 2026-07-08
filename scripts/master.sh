db6187f45151d4d9a0c27a55b9ad9db5313bab6c
RanisAbit <hulkfrombua@gmail.com>
2026-07-08 08:14:19 +0300

    Update sripts

--- scripts/master.sh
+++ scripts/master.sh
@@ -1,30 +1,53 @@
 #! /bin/bash
-
 master_user=repl
+echo "Выполняется настройка master-server"
+sudo apt update -y 
+sudo apt install -y mysql-server 
 
-echo "Настройка master сервера !!!"
+if [ ! -f /etc/mysql/mysql.conf.d/mysqld.cnf.bak ]; then
+sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak
+fi
 
-sudo apt update
-sudo apt install -y mysql-server
+if ! grep -q "server_id = 1" "$config_file"; then
 
 sudo tee /etc/mysql/mysql.conf.d/mysqld.cnf <<EOF
 [mysqld]
-bind-address = 0.0.0.0
+# * Basic Settings
+user            = mysql
+# pid-file      = /var/run/mysqld/mysqld.pid
+# socket        = /var/run/mysqld/mysqld.sock
+# port          = 3306
+# datadir       = /var/lib/mysql
+# Master Settings
+bind-address            = 0.0.0.0
+mysqlx-bind-address     = 127.0.0.1
+key_buffer_size         = 16M
+log_error = /var/log/mysql/error.log
 server_id = 1
-log_bin = mysql-bin
-binlog_format = ROW
-gtid_mode = ON
+log-bin = mysql-bin
+binlog_format = row
+gtid-mode=ON
 enforce_gtid_consistency = ON
-log_replica_updates = ON
-EOF
+log-replica-updates
+max_binlog_size   = 100M
 
+EOF
 sudo systemctl restart mysql
+else
+echo "Конфиг содержит настройки master-server"
+fi
 
 read -rsp "Введите пароль для УЗ подключения к Мастеру: " pass
 echo
 
-sudo mysql -e "CREATE USER '${master_user}'@'%' IDENTIFIED BY '${pass}';"
+if [ -z "$pass" ]; then
+    echo "Пароль не может быть пустым"
+    exit 1
+fi
+
+sudo mysql -e "ALTER USER 'repl'@'%'IDENTIFIED BY '${pass}';"
 sudo mysql -e "GRANT REPLICATION SLAVE ON *.* TO '${master_user}'@'%';"
 sudo mysql -e "FLUSH PRIVILEGES;"
+sudo mysql -e "SHOW MASTER STATUS\G"
 
-sudo mysql -e "SHOW BINARY LOG STATUS\G"
+echo "Master-server настроен"
--- scripts/slave.sh
+++ scripts/slave.sh
@@ -4,17 +4,28 @@ master_user="repl"
 master_address="$1"
 
 if [ -z "$master_address" ]; then
-    echo "Укажи IP адрес master сервера"
+    echo "Передай IP master-сервера аргументом"
     exit 1
 fi
 
-echo "Настройка slave сервера !!!"
+echo "Настройка slave-сервера !!!"
 
 sudo apt update
 sudo apt install -y mysql-server
 
+if [ ! -f /etc/mysql/mysql.conf.d/mysqld.cnf.bak ]; then
+sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.bak
+fi
+
 sudo tee /etc/mysql/mysql.conf.d/mysqld.cnf > /dev/null <<EOF
 [mysqld]
+# * Basic Settings
+user            = mysql
+# pid-file      = /var/run/mysqld/mysqld.pid
+# socket        = /var/run/mysqld/mysqld.sock
+# port          = 3306
+# datadir       = /var/lib/mysql
+# Slave Settings
 server_id = 2
 log_bin = mysql-bin
 relay_log = relay-log-server
@@ -31,8 +42,10 @@ read -rsp "Введите пароль для УЗ подключения к Mas
 echo
 
 sudo mysql -e "STOP REPLICA;"
-sudo mysql -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='${master_address}', SOURCE_USER='${master_user}', SOURCE_PASSWORD='${pass}', SOURCE_AUTO_POSITION=1;"
+sudo mysql -e "CHANGE REPLICATION SOURCE TO SOURCE_HOST='${master_address}', SOURCE_USER='${master_user}', SOURCE_PASSWORD='${pass}', SOURCE_AUTO_POSITION=1, GET_SOURCE_PUBLIC_KEY = 1;"
 sudo mysql -e "START REPLICA;"
 sudo mysql -e "SHOW REPLICA STATUS\\G"
+echo "Настройка slave-server зарешена"
+
 
 
